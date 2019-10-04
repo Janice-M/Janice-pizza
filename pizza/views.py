1b635bf4d47c0ad8c3904a07a38630ffdaa1c704
Janice-M <janiceink001@gmail.com>
2019-10-04 14:30:17 +0300

    markup showing well

--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    '''
+    view to redirect the user to their specific dashboard
+    '''
+
+    return render(request,'index.html')
