438258b326cde60d74db0b168eeda34e4b76c3f6
Janice-M <janiceink001@gmail.com>
2019-10-04 12:39:16 +0300

    js products

---
+++ pizza/static/js/index.js
@@ -0,0 +1,128 @@
+console.log('redbull');
+console.log('bruh');
+console.log(69);
+
+$(document).ready(function() {
+
+  $(".whatwedo p").hide();
+
+  $('.whatwedo img.one').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+  $('.whatwedo p.one').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+  $('.whatwedo img.two').click(function() {
+    $(".whatwedo img.two").toggle();
+    $(".whatwedo p.two").toggle();
+  });
+  $('.whatwedo p.two').click(function() {
+    $(".whatwedo img.two").toggle();
+    $(".whatwedo p.two").toggle();
+  });
+  $('.whatwedo img.three').click(function() {
+    $(".whatwedo img.three").toggle();
+    $(".whatwedo p.three").toggle();
+  });
+  $('.whatwedo p.three').click(function() {
+    $(".whatwedo img.three").toggle();
+    $(".whatwedo p.three").toggle();
+  });
+
+  $('.whatwedo h3').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+  $('.whatwedo h3').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+
+  $("table").hide();
+  $(".additional-buttons").hide();
+  $(".additional-info").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $(".additional-info h4").hide();
+
+  $('.btn.order').click(function() {
+    var sizeOfPizza = $(".size option:selected").val();
+    var toppingsOfPizza = $(".toppings option:selected").val();
+    var crustOfPizza = $(".crust option:selected").val();
+    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+    var order = 1;
+    var grandTotal = 0;
+
+    $("table").show();
+    $(".additional-buttons").show();
+    $(".btn.order").hide();
+
+    $("#size").html($(".size option:selected").text() + " - " + sizeOfPizza);
+    $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
+    $("#crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
+    $("#total").html(total);
+
+    function Pizza(size, toppings, crust, total, orderNo) {
+      this.size = size;
+      this.toppings = toppings;
+      this.crust = crust;
+      this.total = total;
+      this.orderNo = orderNo;
+    }
+
+
+    $('.btn.add-pizza').click(function() {
+      var sizeOfPizza = $(".size option:selected").val();
+      var toppingsOfPizza = $(".toppings option:selected").val();
+      var crustOfPizza = $(".crust option:selected").val();
+      var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+      order = order + 1;
+      grandTotal = grandTotal + total;
+
+
+      var newPizza = new Pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
+
+      var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+
+      $("#pizza").append(newRow);
+    });
+
+    $(".btn.check-out").click(function() {
+      $(".btn.add-pizza").hide();
+      $(".btn.check-out").hide();
+      $(".additional-info").show();
+      $(".btn.yes").show();
+      $(".btn.no").show();
+      $(".additional-info .location").hide();
+      grandTotal = grandTotal + total;
+
+      $(".additional-info h3 span").html(grandTotal);
+    });
+
+    $(".btn.yes").click(function() {
+      $(".additional-info h5").hide();
+      $(".btn.yes").hide();
+      $(".btn.no").hide();
+      $(".additional-info .location").show();
+      $(".additional-info h3 span").html(grandTotal + 200);
+    });
+
+    $(".btn.no").click(function() {
+      $(".additional-info h5").hide();
+      $(".btn.yes").hide();
+      $(".btn.no").hide();
+      $(".additional-info .location").show();
+    });
+
+    $(".btn.complete").click(function() {
+      var location = $(".additional-info .location input").val();
+      $(".additional-info h4").show();
+      $(".additional-info .location").hide();
+      $(".additional-info h4 span").html(location);
+    });
+
+  });
+
+});
